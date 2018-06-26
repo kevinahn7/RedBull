8667e8fd8617342792684448f657f53e6edd4e53
Kevin Ahn and Thad Donaghue <thad@>
2018-06-26 12:49:30 -0700

    Add second test

--- RedBull.Tests/ModelTest/RedBullTest.cs
+++ RedBull.Tests/ModelTest/RedBullTest.cs
@@ -6,10 +6,21 @@ namespace RedBullProject.Tests
     public class RedBullTest
     {
         [TestMethod]
-        public void IsLegitInput_InputIsLegit_True()
+        public void IsColumnLegit_InputIsLegit_True()
         {
             RedBull testRedBull = new RedBull();
-            Assert.AreEqual(true, testRedBull.IsLegitInput("A"));
+            Assert.AreEqual(true, testRedBull.IsColumnLegit(1));
+            Assert.AreEqual(false, testRedBull.IsColumnLegit(10));
+            Assert.AreEqual(false, testRedBull.IsColumnLegit(123));
+        }
+
+        [TestMethod]
+        public void IsRowLegit_InputIsLegit_True()
+        {
+            RedBull testRedBull = new RedBull();
+            Assert.AreEqual(true, testRedBull.IsRowLegit("A"));
+            Assert.AreEqual(true, testRedBull.IsRowLegit("a"));
+            Assert.AreEqual(false, testRedBull.IsRowLegit("5"));
         }
     }
 }
--- RedBull/Models/RedBull.cs
+++ RedBull/Models/RedBull.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RedBullProject
 {
     public class RedBull
     {
+        string[] validRowArray = { "A", "B", "C", "D", "E", "F", "G", "H" };
+        private int Column { set; get; }
+        private string Row { set; get; }
+
+
         public void StartGame()
         {
             this.GetInput();
@@ -11,22 +17,41 @@ namespace RedBullProject
 
         public void GetInput()
         {
-            Console.WriteLine("sdfdsfsd");
-            var inputtedValue = Console.ReadLine();
-            this.IsLegitInput(inputtedValue);
+            Console.WriteLine("Enter a column location for the pawn to go on with a number 1-8");
+            int inputtedColumn = int.Parse(Console.ReadLine());
+            this.IsColumnLegit(inputtedColumn);
+            Console.WriteLine("Enter a row location for the pawn to go on with a letter A-H");
+            string inputtedRow = Console.ReadLine().ToUpper();
+            this.IsRowLegit(inputtedRow);
         }
 
-        public bool IsLegitInput(string input)
+        public void IsColumnLegit(int input)
         {
-            if (input == "A")
+            if (input > 0 && input < 9)
             {
-                return true;
+                
             }
-
-            return false;
+            else 
+            {
+                Console.WriteLine("That is not a valod input");
+                this.GetInput();
+            }
         }
 
-
+        public bool IsRowLegit(string input)
+        {
+            
+            bool valid = false;
+            for (int i = 0; i < validRowArray.Length; i++)
+            {
+                if (validRowArray[i] == input)
+                {
+                    valid = true;
+                    break;
+                }
+            }
+            return valid;
+        }
     }
 
 
